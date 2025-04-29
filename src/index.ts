067b7213ce375d17a6fe01d375e0b1d5c2046167
Gustavo Iha <gustavoiha@gmail.com>
2025-04-29 16:31:00 -0300

    build: add prod webpack build

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import House from "./house"
 console.log("Boilerplate Webpack Server")
 
 const smallHouse = new House(40, 200_000)
-const largeHouse = new House(80, 500_000)
+const largeHouse = new House(80, 501_000)
 
 console.log(largeHouse.pricePerSquareMeter() - smallHouse.pricePerSquareMeter())
---
+++ webpack.prod.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "./"),
+    }
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist",
+    clean: true
+  }
+};
